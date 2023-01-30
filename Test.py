5b483b108e7193b4e2e6d7dbd703f3ac436de097
Sankalp Bhardwaj <54895471+SankalpDoC@users.noreply.github.com>
2023-01-30 17:52:10 +0530

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,15 @@
+import unittest
+
+from Prog1.py import summation
+
+class TestSum(unittest.TestCase) :
+  def test_list_int(self) :
+    """
+    Test Case To Add Two Numbers
+    """
+    data = [32, 23]
+    result = summation(data)
+    self.assertEqual(result, 55)
+    
+if __name__ == '__main__' :
+  unittest.main()
